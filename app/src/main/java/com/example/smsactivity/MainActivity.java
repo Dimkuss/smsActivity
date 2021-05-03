2128907ebc315e5eedae1d10618a125978a4c4c4
Mokhov Dmitriy <mohow-95@mail.ru>
2021-05-03 21:55:17 +0300

    fix bugs with call and sms

--- app/src/main/java/com/example/smsactivity/MainActivity.java
+++ app/src/main/java/com/example/smsactivity/MainActivity.java
@@ -16,6 +16,7 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     private static final int MY_PERMISSIONS_REQUEST_CALL_PHONE = 11;
+    private static final int MY_PERMISSIONS_REQUEST_SMS = 12;
     EditText textCall;
     EditText textSMS;
     Button btnCall;
@@ -26,13 +27,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         callByNumber();
+        sendSmsText();
     }
 
     public void callByNumber() {
         btnCall = findViewById(R.id.btnCall);
-        btnSMS = findViewById(R.id.buttonSMS);
-        textCall = findViewById(R.id.EditTextCall);
-        textSMS = findViewById(R.id.EditTextSMS);
+        textCall = findViewById(R.id.editTextCall);
         btnCall.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -41,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
                     ActivityCompat.requestPermissions
                             (MainActivity.this, new String[]{Manifest.permission.CALL_PHONE}, MY_PERMISSIONS_REQUEST_CALL_PHONE);
                 } else {
-                    String number = textCall.toString();
+                    String number = textCall.getText().toString();
                     Uri call =Uri.parse("tel:" + number);
                     Intent intent = new Intent(Intent.ACTION_CALL,call);
 
@@ -59,6 +59,46 @@ public class MainActivity extends AppCompatActivity {
             }
 
 
+        });
+
+    }
+    public void sendSmsText(){
+        btnSMS = findViewById(R.id.buttonSMS);
+        textSMS = findViewById(R.id.editTextSMS);
+        textCall = findViewById(R.id.editTextCall);
+        btnSMS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (ContextCompat.checkSelfPermission
+                        (MainActivity.this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions
+                            (MainActivity.this, new String[]{Manifest.permission.SEND_SMS}, MY_PERMISSIONS_REQUEST_SMS);
+                } else {
+                    String number = textCall.getText().toString();
+
+                    String text = textSMS.getText().toString();
+
+                    Intent intent = new Intent(Intent.ACTION_VIEW);
+                    intent.setData(Uri.parse("smsto:" + number)) ;
+                    intent.setType("vnd.android-dir/mms-sms");
+                    intent.putExtra("address", number);
+                    intent.putExtra("sms_body", text);
+
+
+
+
+//                    intent.setData(Uri.parse("tel: "+ number));
+                    if (intent.resolveActivity(getPackageManager())!=null){
+                        startActivity(intent.createChooser(intent,"Send SMS with "));
+                    }
+                    else {
+                        Log.d("Intent","ActivityNotFound");
+                    }
+
+                }
+
+            }
         });
 
     }
